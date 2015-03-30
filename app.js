1bc7535ae81b809a61128e39ce679335c5d4c3b7
Hector Leiva <hector@hectorleiva.com>
2015-03-30 00:16:01 -0400

    attempting promises.

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-var http        = require('http');
-    XmlStream   = require('xml-stream');
-    url         = require('url');
-    fs          = require('fs');
-    validate    = require('url-validator');
-    argv        = require('minimist')(process.argv.slice(2));
-    Crawler     = require('simplecrawler');
+var http        = require('http'),
+    XmlStream   = require('xml-stream'),
+    url         = require('url'),
+    fs          = require('fs'),
+    validate    = require('url-validator'),
+    argv        = require('minimist')(process.argv.slice(2)),
+    Q           = require('q'),
+    Crawler     = require('simplecrawler'),
+    writeFile   = Q.denodeify(fs.writeFile);
 
-var crawl
-    , index_sitemap
-    , format
-    , crawler
-    , res_data
-    , http_get_options
-    , rendered_sitemaps_folder
-    , conditionID
+var crawl,
+    index_sitemap,
+    format,
+    crawler,
+    res_data,
+    http_get_options,
+    rendered_sitemaps_folder,
+    conditionID;
 
 /**
  * The Index Sitemap that will be crawled for more links
  * @type {String} URL to the sitemap.
  */
 if (argv.site === undefined) {
-    console.log("'sitemap_index_url' is unspecified, please use the argument --sitemap_index_url= to designate the address to the sitemap url");
-    process.exit();
+    var errorMsg = "'sitemap_index_url' is unspecified, please use the argument --sitemap_index_url= to designate the address to the sitemap url";
+    throw new Error(errorMsg);
 }
 
 //  Validate if it is a valid URL
 index_sitemap = validate(argv.site);
 if (index_sitemap === false) {
-    console.log("Enter a proper URL including http:// pointing to your sitemap index");
-    process.exit();
+    var errorMsg = "Enter a proper URL including http:// pointing to your sitemap index";
+    throw new Error(errorMsg);
 }
 
 switch (argv.format) {
@@ -46,6 +48,46 @@ switch (argv.format) {
 console.log("Format set to " + format + ".");
 console.log("Crawling: " + index_sitemap + '\n');
 
+var httpGet = function(opts) {
+  var deferred = Q.defer();
+  http.get(opts, deferred.resolve);
+  return deferred.promise;
+};
+
+var loadBody = function(res) {
+  var deferred = Q.defer();
+  var body = '';
+  res.on("data", function(chunk) {
+    body += chunk;
+  });
+  res.on("end", function() {
+    deferred.resolve(body);
+  });
+  return deferred.promise;
+}
+
+function jobCrawler(url_feed, format) {
+  if (format == 'xml') {
+    httpGet(url_feed).then(function(res) {
+      res.setEncoding('utf8');
+      return res;
+    }).then(function(res) {
+      loadBody(res).then(function(body) {
+        sitemap_filename = rendered_sitemaps_folder + url.parse(url_feed).pathname;
+        var ff = format_file(sitemap_filename, format);
+        return Q.nfcall(fs.writeFile, ff, body)
+          .then(function(result) {
+            console.log('the file is at : ', result);
+          })
+          .fail(function(err) {
+            console.log('well shit: ', err);
+          })
+          .done();
+      });
+    });
+  }
+};
+
 crawler = Crawler.crawl(index_sitemap);
 crawler.interval = 2000; // 1000 = 1 second
 
@@ -61,7 +103,7 @@ if (!fs.existsSync(rendered_sitemaps_folder)) {
 //  Crawler engage
 crawler.on("fetchcomplete", function(queueItem, resBuffer, response) {
     console.log("crawler has received %s (%d bytes)",queueItem.url,resBuffer.length);
-    job_crawler(queueItem.url);
+    jobCrawler(queueItem.url, format);
 });
 
 //  Only parse XML documents and ignore all other links
@@ -74,56 +116,39 @@ crawler.on("complete", function(err, response) {
     console.log("Crawler has completed crawling the sitemap index.");
 });
 
+//function job_crawler(url_feed) {
+//    if (format == 'xml') {
+//        //  Just pass in the vanilla XML to be saved as a file
+//    } else {
+//        var request = http.get(url_feed).on('response', function(response) {
+//            response.setEncoding('utf8');
+//            var xml = new XmlStream(response);
+//
+//            /**
+//            * Specify which element within the XML response that you want to have
+//            * parsed back and placed into the array to save it within the CSV.
+//            *
+//            * The current default is: <loc></loc>
+//            */
+//            xml.on('endElement: loc', function(xml_item) {
+//                res_data.push(xml_item.loc + '\n');
+//            });
+//
+//            xml.on('end', function(item) {
+//                sitemap_filename = rendered_sitemaps_folder + url.parse(url_feed).pathname;
+//                process_file(sitemap_filename, res_data, format);
+//
+//                //  Reset the array
+//                res_data = [];
+//
+//            });
+//        }); // end of the http request
+//    }
+//}
 
-function job_crawler(url_feed) {
-    if (format == 'xml') {
-        //  Just pass in the vanilla XML to be saved as a file
-        http.get(url_feed, function(res) {
-            res.setEncoding('utf8');
-            res.on('data', function(chunk) {
-                res_data += chunk;
-            });
-            res.on('end', function() {
-                sitemap_filename = rendered_sitemaps_folder + url.parse(url_feed).pathname;
-                process_file(sitemap_filename, res_data, format);
-                //  Reset the res_data for the new sitemap
-                res_data = '';
-            });
-        });
-    } else {
-        var request = http.get(url_feed).on('response', function(response) {
-            response.setEncoding('utf8');
-            var xml = new XmlStream(response);
-
-            /**
-            * Specify which element within the XML response that you want to have
-            * parsed back and placed into the array to save it within the CSV.
-            *
-            * The current default is: <loc></loc>
-            */
-            xml.on('endElement: loc', function(xml_item) {
-                res_data.push(xml_item.loc + '\n');
-            });
-
-            xml.on('end', function(item) {
-                sitemap_filename = rendered_sitemaps_folder + url.parse(url_feed).pathname;
-                process_file(sitemap_filename, res_data, format);
-
-                //  Reset the array
-                res_data = [];
-
-            });
-        }); // end of the http request
-    }
-}
-
-function process_file(sitemap_filename, data, format) {
-
+function format_file(sitemap_filename, format) {
     if (format !== 'xml') {
         sitemap_filename = sitemap_filename.replace(/xml/g, format);
     }
-    fs.writeFile(sitemap_filename, data, function(err) {
-        if (err) throw err;
-    });
-    console.log("processed " + format + ", it is now located in : " + sitemap_filename);
+    return sitemap_filename;
 }
